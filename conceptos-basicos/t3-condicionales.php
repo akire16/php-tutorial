36b82cfe04f906d9bd2856c5b88f6ea0c62b5a4c
akire <aketmi@gmail.com>
2017-01-08 15:03:18 -0500

    tutorial 3, condicionales

---
+++ conceptos-basicos/t3-condicionales.php
@@ -0,0 +1,23 @@
+<?php 
+
+	/*
+		Hacer un programa que lea la edad de una persona y determine si es mayor de edad o no.
+
+		Sintáxis de la condicion si : 
+
+			if ( condición ) {
+				expresiones
+			} else {
+				expresiones
+			}
+	*/
+
+	$edad = 18;
+
+	if ( $edad > 17 ) {
+		echo "La persona es mayor de edad";
+	} else {
+		echo "La persona es menor de edad";
+	}
+
+?>
